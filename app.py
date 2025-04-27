bed7c2d79291a9b6b23f3b41a4febd76f54330b9
Mohammad Abdul Rawoof <rafiraj78616@gmail.com>
2025-04-27 10:06:34 +0530

    updated changes

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 import math
 
@@ -58,4 +59,5 @@ def get_template():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0',port=port)
